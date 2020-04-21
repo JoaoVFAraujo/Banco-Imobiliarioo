2d31dc700dfb7ee243c5ff71d948a1309706601d
joao.victor <joao.victor@f13.com.br>
2020-04-21 12:07:45 -0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrMaskerModule } from 'br-mask';
 import { TransferComponent } from './transfer/transfer.component';
 import { DatabaseProvider } from './services/database/database';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [
     DatabaseProvider,
